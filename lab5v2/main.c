925f6abf51fd1f7d3f86e68524cf498519b4d6ae
zoxal <zzoxall@gmail.com>
2018-04-23 13:17:09 +0300

    lab5v2

---
+++ lab5v2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <CL/cl.h>
+
+#define MAX_PLATFORMS	2
+/*
+1. Calculate integral image in each work group.
+
+*/
+int main(int args, char *argv[]) 
+{
+	// Create OpenCL Context
+	cl_platform platforms[MAX_PLATFOR];
+	printf("Integral image calculations\n");	
+	return 0;
+}
